61a4cc1269b5ba6192b257ef86ccf70a703fd1c4
baldo <baldo@toppoint.de>
2022-05-23 10:49:09 +0200

    Fix: Workaround GLIBC not found issue in Webstorm

--- nix/lib.inc.sh
+++ nix/lib.inc.sh
@@ -6,5 +6,6 @@ function wrap() {
 
     SHELL_NIX="$REPO/shell.nix"
     QUOTED_ARGS="$(printf "${1+ %q}" "$@")"
+    export LD_LIBRARY_PATH=
     exec nix-shell "$SHELL_NIX" --pure --run "$TOOL $QUOTED_ARGS"
 }
