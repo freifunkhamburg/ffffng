cb4bb9e817b1911cf7b8fb0bb77d43b63869c4bb
baldo <baldo@toppoint.de>
2022-05-30 13:56:24 +0200

    Remove redundant main tags.

--- frontend/src/views/AdminDashboardView.vue
+++ frontend/src/views/AdminDashboardView.vue
@@ -12,53 +12,51 @@ refresh();
 </script>
 
 <template>
-    <main>
-        <div v-if="statistics.getStatistics">
-            <h2>Knotenstatistik</h2>
+    <div v-if="statistics.getStatistics">
+        <h2>Knotenstatistik</h2>
 
-            <div class="statistics">
-                <StatisticsCard
-                    title="Registrierte Knoten"
-                    icon="circle-o"
-                    variant="info"
-                    :value="statistics.getStatistics.nodes.registered"
-                    link="/admin/nodes"
-                    />
-                <StatisticsCard
-                    title="Mit hinterlegtem fastd-Key"
-                    icon="lock"
-                    variant="warning"
-                    :value="statistics.getStatistics.nodes.withVPN"
-                    link="/admin/nodes"
-                    :filter="{hasKey: true}"
-                    />
-                <StatisticsCard
-                    title="Mit Koordinaten"
-                    icon="map-marker"
-                    variant="success"
-                    :value="statistics.getStatistics.nodes.withCoords"
-                    link="/admin/nodes"
-                    :filter="{hasCoords: true}"
-                    />
-                <StatisticsCard
-                    title="Monitoring aktiv"
-                    icon="heartbeat"
-                    variant="success"
-                    :value="statistics.getStatistics.nodes.monitoring.active"
-                    link="/admin/nodes"
-                    :filter="{monitoringState: 'active'}"
-                    />
-                <StatisticsCard
-                    title="Monitoring noch nicht bestätigt"
-                    icon="envelope"
-                    variant="danger"
-                    :value="statistics.getStatistics.nodes.monitoring.pending"
-                    link="/admin/nodes"
-                    :filter="{monitoringState: 'pending'}"
-                    />
-            </div>
+        <div class="statistics">
+            <StatisticsCard
+                title="Registrierte Knoten"
+                icon="circle-o"
+                variant="info"
+                :value="statistics.getStatistics.nodes.registered"
+                link="/admin/nodes"
+                />
+            <StatisticsCard
+                title="Mit hinterlegtem fastd-Key"
+                icon="lock"
+                variant="warning"
+                :value="statistics.getStatistics.nodes.withVPN"
+                link="/admin/nodes"
+                :filter="{hasKey: true}"
+                />
+            <StatisticsCard
+                title="Mit Koordinaten"
+                icon="map-marker"
+                variant="success"
+                :value="statistics.getStatistics.nodes.withCoords"
+                link="/admin/nodes"
+                :filter="{hasCoords: true}"
+                />
+            <StatisticsCard
+                title="Monitoring aktiv"
+                icon="heartbeat"
+                variant="success"
+                :value="statistics.getStatistics.nodes.monitoring.active"
+                link="/admin/nodes"
+                :filter="{monitoringState: 'active'}"
+                />
+            <StatisticsCard
+                title="Monitoring noch nicht bestätigt"
+                icon="envelope"
+                variant="danger"
+                :value="statistics.getStatistics.nodes.monitoring.pending"
+                link="/admin/nodes"
+                :filter="{monitoringState: 'pending'}"
+                />
         </div>
-    </main>
+    </div>
 </template>
 
 <style lang="scss" scoped>
--- frontend/src/views/AdminNodesView.vue
+++ frontend/src/views/AdminNodesView.vue
@@ -41,126 +41,124 @@ refresh();
 </script>
 
 <template>
-    <main>
-        <h2>Knoten</h2>
+    <h2>Knoten</h2>
 
-        <div>
-            <span>Gesamt: {{nodes.getTotalNodes}}</span>
-        </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Besitzer*in</th>
-                    <th>E-Mail</th>
-                    <th>Token</th>
-                    <th>MAC</th>
-                    <th>VPN</th>
-                    <th>Site</th>
-                    <th>Domäne</th>
-                    <th>GPS</th>
-                    <th>Status</th>
-                    <th>Monitoring</th>
-                </tr>
-            </thead>
+    <div>
+        <span>Gesamt: {{nodes.getTotalNodes}}</span>
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Besitzer*in</th>
+                <th>E-Mail</th>
+                <th>Token</th>
+                <th>MAC</th>
+                <th>VPN</th>
+                <th>Site</th>
+                <th>Domäne</th>
+                <th>GPS</th>
+                <th>Status</th>
+                <th>Monitoring</th>
+            </tr>
+        </thead>
 
-            <tbody>
-                <tr
-                    v-for="node in nodes.getNodes"
-                    :class="[hasOnlineState ? node.onlineState.toLowerCase() : 'online-state-unknown']">
-                    <td>{{node.hostname}}</td>
-                    <td v-if="shallRedactField(node, 'nickname')">
-                        <span
-                            class="redacted"
-                            @click="setRedactField(node, 'nickname', false)">
-                            nickname
-                        </span>
-                    </td>
-                    <td v-if="!shallRedactField(node, 'nickname')">
-                        <span
-                            class="redactable"
-                            @click="setRedactField(node, 'nickname', true)">
-                            {{node.nickname}}
-                        </span>
-                    </td>
-                    <td v-if="shallRedactField(node, 'email')">
-                        <span
-                            class="redacted"
-                            @click="setRedactField(node, 'email', false)">
-                            email@example.com
-                        </span>
-                    </td>
-                    <td v-if="!shallRedactField(node, 'email')">
-                        <span
-                            class="redactable"
-                            @click="setRedactField(node, 'email', true)">
-                            {{node.email}}
-                        </span>
-                    </td>
-                    <td v-if="shallRedactField(node, 'token')">
-                        <span
-                            class="redacted"
-                            @click="setRedactField(node, 'token', false)">
-                            0123456789abcdef
-                        </span>
-                    </td>
-                    <td v-if="!shallRedactField(node, 'token')">
-                        <span
-                            class="redactable"
-                            @click="setRedactField(node, 'token', true)">
-                            {{node.token}}
-                        </span>
-                    </td>
-                    <td>{{node.mac}}</td>
-                    <td class="icon">
-                        <i
-                            v-if="node.key"
-                            class="fa fa-lock"
-                            aria-hidden="true"
-                            title="Hat VPN-Schlüssel" />
-                        <i
-                            v-if="!node.key"
-                            class="fa fa-times not-available"
-                            aria-hidden="true"
-                            title="Hat keinen VPN-Schlüssel" />
-                    </td>
-                    <td>{{node.site}}</td>
-                    <td>{{node.domain}}</td>
-                    <td class="icon">
-                        <i
-                            v-if="node.coords"
-                            class="fa fa-map-marker"
-                            aria-hidden="true"
-                            title="Hat Koordinaten" />
-                        <i
-                            v-if="!node.coords"
-                            class="fa fa-times not-available"
-                            aria-hidden="true"
-                            title="Hat keinen Koordinaten" />
-                    </td>
-                    <td v-if="node.onlineState !== undefined">{{node.onlineState.toLowerCase()}}</td>
-                    <td v-if="node.onlineState === undefined">unbekannt</td>
-                    <td class="icon">
-                        <i
-                            v-if="node.monitoring && node.monitoringConfirmed"
-                            class="fa fa-heartbeat"
-                            aria-hidden="true"
-                            title="Monitoring aktiv" />
-                        <i
-                            v-if="node.monitoring && !node.monitoringConfirmed"
-                            class="fa fa-envelope"
-                            aria-hidden="true"
-                            title="Monitoring nicht bestätigt" />
-                        <i
-                            v-if="!node.monitoring"
-                            class="fa fa-times not-available"
-                            aria-hidden="true"
-                            title="Monitoring deaktiviert" />
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </main>
+        <tbody>
+            <tr
+                v-for="node in nodes.getNodes"
+                :class="[hasOnlineState ? node.onlineState.toLowerCase() : 'online-state-unknown']">
+                <td>{{node.hostname}}</td>
+                <td v-if="shallRedactField(node, 'nickname')">
+                    <span
+                        class="redacted"
+                        @click="setRedactField(node, 'nickname', false)">
+                        nickname
+                    </span>
+                </td>
+                <td v-if="!shallRedactField(node, 'nickname')">
+                    <span
+                        class="redactable"
+                        @click="setRedactField(node, 'nickname', true)">
+                        {{node.nickname}}
+                    </span>
+                </td>
+                <td v-if="shallRedactField(node, 'email')">
+                    <span
+                        class="redacted"
+                        @click="setRedactField(node, 'email', false)">
+                        email@example.com
+                    </span>
+                </td>
+                <td v-if="!shallRedactField(node, 'email')">
+                    <span
+                        class="redactable"
+                        @click="setRedactField(node, 'email', true)">
+                        {{node.email}}
+                    </span>
+                </td>
+                <td v-if="shallRedactField(node, 'token')">
+                    <span
+                        class="redacted"
+                        @click="setRedactField(node, 'token', false)">
+                        0123456789abcdef
+                    </span>
+                </td>
+                <td v-if="!shallRedactField(node, 'token')">
+                    <span
+                        class="redactable"
+                        @click="setRedactField(node, 'token', true)">
+                        {{node.token}}
+                    </span>
+                </td>
+                <td>{{node.mac}}</td>
+                <td class="icon">
+                    <i
+                        v-if="node.key"
+                        class="fa fa-lock"
+                        aria-hidden="true"
+                        title="Hat VPN-Schlüssel" />
+                    <i
+                        v-if="!node.key"
+                        class="fa fa-times not-available"
+                        aria-hidden="true"
+                        title="Hat keinen VPN-Schlüssel" />
+                </td>
+                <td>{{node.site}}</td>
+                <td>{{node.domain}}</td>
+                <td class="icon">
+                    <i
+                        v-if="node.coords"
+                        class="fa fa-map-marker"
+                        aria-hidden="true"
+                        title="Hat Koordinaten" />
+                    <i
+                        v-if="!node.coords"
+                        class="fa fa-times not-available"
+                        aria-hidden="true"
+                        title="Hat keinen Koordinaten" />
+                </td>
+                <td v-if="node.onlineState !== undefined">{{node.onlineState.toLowerCase()}}</td>
+                <td v-if="node.onlineState === undefined">unbekannt</td>
+                <td class="icon">
+                    <i
+                        v-if="node.monitoring && node.monitoringConfirmed"
+                        class="fa fa-heartbeat"
+                        aria-hidden="true"
+                        title="Monitoring aktiv" />
+                    <i
+                        v-if="node.monitoring && !node.monitoringConfirmed"
+                        class="fa fa-envelope"
+                        aria-hidden="true"
+                        title="Monitoring nicht bestätigt" />
+                    <i
+                        v-if="!node.monitoring"
+                        class="fa fa-times not-available"
+                        aria-hidden="true"
+                        title="Monitoring deaktiviert" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <style lang="scss" scoped>
