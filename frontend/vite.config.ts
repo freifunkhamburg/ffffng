403a53786961bb1592ef1cf34deb4cca294e3828
baldo <baldo@toppoint.de>
2022-07-14 11:26:43 +0200

    Fixed vite ssl.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,21 @@
-import { fileURLToPath, URL } from "url";
+import {fileURLToPath, URL} from "url";
 
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [basicSsl(), vue()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
     server: {
+        https: true,
+        port: 3000,
+        strictPort: true,
         proxy: {
             "/api/": {
                 target: "http://localhost:8080",
