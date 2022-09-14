894ee97fdfc32e4d2c3b3cbd26f8c1f017ee7b1e
baldo <baldo@toppoint.de>
2022-09-14 15:56:16 +0200

    Update server dependencies.

--- jest.server.config.js
+++ jest.server.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/server'],
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/server/tsconfig.json'
-        }
-    }
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/server"],
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                tsconfig: "<rootDir>/server/tsconfig.json",
+            },
+        ],
+    },
 };
